019cd80c8d9450e2f558d95d9bed2ac69953e4e8
Megan V <elbowgrrl@gmail.com>
2021-08-24 13:44:16 -0700

    adding initial solution to repeating-numbers

--- repeating-numbers.js
+++ repeating-numbers.js
@@ -10,13 +10,21 @@ const repeatingNumbers = function (data) {
 
       let str = `${num}`;
       return str.repeat(reps);
+    } else {
+      const num = array[0];
+      const reps = array[1];
+      // console.log(array)
+      let str = `${num}`;
+      superStr = str.repeat(reps);
+      output += superStr.concat(", ")
     }
+    
   }
 
   //confirm that output is string and print
-  // return output;
+  return output.slice(0, -2);
 };
 
-console.log(repeatingNumbers([[1, 10]]));
-// console.log(repeatingNumbers([[10, 4], [34, 6], [92, 2]]));
-// console.log(repeatingNumbers([[17, 34]]));
+// console.log(repeatingNumbers([[1, 10]]));
+console.log(repeatingNumbers([[10, 4], [34, 6], [92, 2]]));
+console.log(repeatingNumbers([[17, 34]]));
